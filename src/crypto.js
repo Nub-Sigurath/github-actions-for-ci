779dbf2a56cad9ae6fb712d6c695ee3533cc8d51
nyarlatotep <danielcrespo@cresalex.com>
2021-07-24 03:48:27 -0500

    Update crypto.js

--- src/crypto.js
+++ src/crypto.js
@@ -1,18 +1,22 @@
-// Return a random integer between a min and max value.
- function RandomInteger(min,  max)
- {
-    scale = uint.MaxValue;
-    while (scale == uint.MaxValue)
-    {
-      // Get four random bytes.
-      byte[GetBytes] = new byte[4];
-        Rand.GetBytes(four_bytes);
-    }
-
-    // Add min to the scaled difference between max and min.
-    return (min + (max - min) *
-        (scale / 2*uint.MaxValue));
- }
+function RandomInteger(min,max)
+{
+  scale = max
+  while (scale == max) {
+    // Get four random bytes.
+    byte = (new byte[4])
+  }
+  // Add min to the scaled difference between max and min.
+  return (min + (max - min) * (scale / 2 * max))
+}
 // The random number provider.
-var Rand = RNGCryptoServiceProvider(byte[4], byte[0] )
- new RNGCryptoServiceProvider();
+var Rand = RandomInteger(byte[4], byte[0])
+
+function RNGCryptoServiceProvider()
+{
+  return Rand
+}
+
+if (RNGCryptoServiceProvider) {
+  RNGCryptoServiceProvider = RNGCryptoServiceProvider(Rand)
+    (new RNGCryptoServiceProvider())
+}
